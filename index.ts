08bd279ad6b044d69055c70970dc6aa358ad9772
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-25 23:15:25 +0300

    OK

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(sslRedirect());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello brain dead of m");
 });
 
 app.listen(process.env.PORT || 3000);
