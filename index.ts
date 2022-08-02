48d9f701b082d8c4c5d8a2826ea3ef38f41bfde4
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-02 11:55:54 +0300

    Server

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 import sslRedirect from "heroku-ssl-redirect";
 import express from "express";
+import cors from "cors";
 const app = express();
+const port = process.env.PORT || 6444;
 
 app.use(sslRedirect());
+app.listen(port, () => console.log(`Server started on port: ${port}`));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://app.neurobica.online"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello brain dead of m");
 });
-
-app.listen(process.env.PORT || 3000);
