ecd76c82169e443c1c28cd99bf1cdaf2e4cf64ff
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-04 01:42:19 +0300

    Mongo and mongo oc

--- index.ts
+++ index.ts
@@ -1,8 +1,37 @@
 import sslRedirect from "heroku-ssl-redirect";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose, { Connection, ConnectOptions, Mongoose } from "mongoose";
+import winston from "winston";
+import "winston-mongodb";
+import { MongoDBConnectionOptions } from "winston-mongodb";
 const app = express();
 const port = process.env.PORT || 6444;
+
+dotenv.config();
+
+mongoose.connect(
+  "" + process.env.MONGO,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions,
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
+
+winston.add(
+  new winston.transports.MongoDB({
+    db: "" + process.env.MONGO_OC,
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  })
+);
 
 app.use(sslRedirect());
 app.listen(port, () => console.log(`Server started on port: ${port}`));
