c946b0dc3fae525bbddf4fd54c5ae1986573407a
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-06 14:30:16 +0300

    wgatis

--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/loggedIn", async (req, res) => {
   try {
+    console.log(req);
   } catch (err) {
     console.error(err);
     res.status(500).send();
@@ -13,6 +14,7 @@ router.post("/loggedIn", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
+    console.log(req);
   } catch (err) {
     console.error(err);
     res.status(500).send();
