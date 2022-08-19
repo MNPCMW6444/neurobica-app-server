cf2394803ca0d6cbed5190e2d763b012447cc944
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-19 17:02:15 +0300

    Update userRouter.ts

--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,23 +1,67 @@
 import express from "express";
 import User from "../models/userModel";
-
 const router = express.Router();
 
-router.post("/loggedIn", async (req, res) => {
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    console.log(req);
+    if (!email || !password)
+      res.status(400).json({ clientError: "Wrong email or password" });
   } catch (err) {
     console.error(err);
-    res.status(500).send();
+    res
+      .status(500)
+      .json({ serverError: "Unexpected error occurred in the server" });
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(401).json({
+        errorMessage: "Wrong email or password.",
+      });
+
+    const correctPassword = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+
+    if (!correctPassword)
+      return res.status(401).json({
+        errorMessage: "Wrong email or password.",
+      });
+
+    // create a JWT token
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        sameSite:
+          process.env.NODE_ENV === "development"
+            ? "lax"
+            : process.env.NODE_ENV === "production" && "none",
+        secure:
+          process.env.NODE_ENV === "development"
+            ? false
+            : process.env.NODE_ENV === "production" && true,
+      })
+      .send();
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     console.log(req);
   } catch (err) {
     console.error(err);
-    res.status(500).send();
+    res
+      .status(500)
+      .json({ serverError: "Unexpected error occurred in the server" });
   }
 });
 
