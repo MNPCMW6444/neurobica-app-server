98cbb1595e4e5b0f3e97cab426c84199cc9fa601
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-27 12:45:59 +0300

    עםםג מם׳

--- client/src/components/MyAccount/MyAccount.tsx
+++
@@ -1,7 +0,0 @@
-import useUser from "../../hooks/useUser";
-import LoginPage from "../LoginPage/LoginPage";
-
-export default function MyAccount() {
-  const user = useUser();
-  return user ? <div>Hi {user.fullname + "!!!"}</div> : <LoginPage />;
-}
--- client/src/util/NeurobicaColors.tsx
+++
@@ -1,7 +0,0 @@
-enum NeurobicaColors {
-  Main = "#F9A645",
-  Second = "#000000",
-  Third = "#FFFFFF",
-  Forth = "#808080",
-}
-export default NeurobicaColors;
